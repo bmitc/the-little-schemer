6f4cc21338b352728a3fc577261e2dd52283ef94
bmitc <65685447+bmitc@users.noreply.github.com>
2020-12-11 22:51:33 -0600

    Chapter 1 completely revised and completed
    Now all annotations are done via RackUnit tests, and implementations of the book's described atom?, cons, null?, and eq? are provided using Racket's contract system.

--- source/chapter-1.rkt
+++ source/chapter-1.rkt
@@ -1,108 +1,186 @@
+;;**********************************************************
+;; Chapter 1: Toys
+;;**********************************************************
+
 #lang racket
 
-(require "the-little-schemer.rkt")
+(require "the-little-schemer.rkt"
+         rackunit)
 
-'atom
+(check-true (atom? 'atom))
 
-'turkey
+(check-true (atom? 'turkey))
 
-'1942
+(check-true (atom? 1942))
 
-'u
+(check-true (atom? 'u))
 
-'*abc$
+(check-true (atom?  '*abc$))
 
-'(atom)
+(check-true (list? '(atom)))
 
-'(atom turkey or)
+(check-true (list? '(atom turkey or)))
 
-'((atom turkey) or)
+(check-exn exn:fail? (thunk (atom? '(atom turkey) 'or)))
 
-'xyz
+(check-true (list? '((atom turkey) or)))
 
-'(x y z)
+(check-true (s-exp? 'xyz))
 
-'((x y) z)
+(check-true (s-exp? '(x y z)))
 
-'(how are you doing so far)
+(check-true (s-exp? '((x y) z)))
 
-'(((how) are) ((you) (doing so)) far)
+(check-true (list? '(how are you doing so far)))
 
-'()
+(check-equal? (length '(how are you doing so far)) 6)
 
-'(() () () ())
+(check-true (list? '(((how) are) ((you) (doing so)) far)))
 
-(car '(a b c))
+(check-equal? (length '(((how) are) ((you) (doing so)) far)) 3)
 
-(car '((a b c) x y z))
+(check-true (list? '()))
 
+(check-false (atom? '()))
+
+(check-true (list? '(() () () ())))
+
+(check-equal? (car '(a b c)) 'a)
+
+(check-equal? (car '((a b c) x y z)) '(a b c))
+
+(check-exn exn:fail? (thunk (car 'hotdog)))
+
+(check-exn exn:fail? (thunk (car '())))
+
+;;**********************************************************
+;; The Law of Car
+;;
+;; The primitive car is defined only for non-empty lists.
+;;**********************************************************
+
+(check-equal? (car '(((hotdogs)) (and) (pickle) relish))
+              '((hotdogs)))
+
+(check-equal? (car (car '(((hotdogs)) (and))))
+              '(hotdogs))
+
+(check-equal? (cdr '(a b c))
+              '(b c))
+
+(check-equal? (cdr '((a b c) x y z))
+              '(x y z))
+
+(check-equal? (cdr '(hamburger))
+              '())
+
+(check-equal? (cdr '((x) t r))
+              '(t r))
+
+(check-exn exn:fail? (thunk (cdr 'hotdogs)))
+
+(check-exn exn:fail? (thunk (cdr '())))
+
+;;**********************************************************
+;; The Law of Cdr
+;;
+;; The primitive cdr is defined only for non-empty lists.
+;; The cdr of any non-empty list is always another list.
+;;**********************************************************
+
+(check-equal? (car (cdr '((b) (x y) ((c)))))
+              '(x y))
+
+(check-equal? (cdr (cdr '((b) (x y) ((c)))))
+              '(((c))))
+
+(check-exn exn:fail? (thunk (cdr (car '(a (b (c)) d)))))
+
+(check-equal? (cons 'peanut '(butter and jelly))
+              '(peanut butter and jelly))
+
+(check-equal? (cons '(banana and) '(peanut butter and jelly))
+              '((banana and) peanut butter and jelly))
+
+(check-equal? (cons '((help) this) '(is very ((hard) to learn)))
+              '(((help) this) is very ((hard) to learn)))
+
+(check-equal? (cons '(a b (c)) '())
+              '((a b (c))))
+
+(check-equal? (cons 'a '())
+              '(a))
+
+(check-exn exn:fail? (thunk (cons '((a b c)) 'b)))
+;; As alluded to in footnote 1 on page 8, in Racket's cons accepts
+;; any value in both arguments and does not require the second one
+;; to be a list. cons has been redefined in the-little-schemer module
+;; such that it does require a list to follow the book's definition
+;; of cons.
+
+(check-exn exn:fail? (thunk (cons 'a 'b)))
+;; Similar to the previous test.
+
+;;**********************************************************
+;; The Law of Cons
+;;
+;; The primitive cons takes two arguments. The second argument
+;; to cons must be a list. The result is a list.
 ;**********************************************************
-;The Law of Car
-;
-;The primitive car is defined only for non-empty lists.
+
+(check-equal? (cons 'a (car '((b) c d)))
+              '(a b))
+
+(check-equal? (cons 'a (cdr '((b) c d)))
+              '(a c d))
+
+(check-true (null? '()))
+
+(check-true (null? (quote ())))
+
+(check-false (null? '(a b c)))
+
+(check-exn exn:fail? (thunk (null? 'spaghetti)))
+
+;;**********************************************************
+;; The Law of Null
+;;
+;; The primitive null? is defined only for lists.
+;;**********************************************************
+
+(check-true (atom? 'Harry))
+
+(check-false (atom? '(Harry had a heap of apples)))
+
+(check-true (atom? (car '(Harry had a heap of apples))))
+
+(check-false (atom? (cdr '(Harry had a heap of apples))))
+
+(check-false (atom? (cdr '(Harry))))
+
+(check-true (atom? (car (cdr '(swing low sweet cherry oat)))))
+
+(check-false (atom? (car (cdr '(swing (low sweet) cherry oat)))))
+
+(check-true (eq? 'Harry 'Harry))
+
+(check-false (eq? 'margarine 'butter))
+
+(check-exn exn:fail? (thunk (eq? '() '(strawberry))))
+;; eq? has been redefined to only take in non-numeric atoms
+
+(check-exn exn:fail? (thunk (eq? 6 7)))
+
+;;**********************************************************
+;; The Law of Eq?
+;;
+;; The primitive eq? takes two arguments. Each must be
+;; a non-numeric atom.
 ;**********************************************************
 
-(car '(((hotdogs)) (and) (pickle) relish))
+(check-true (eq? (car '(Mary had a little lamb chop)) 'Mary))
 
-(car (car '(((hotdogs)) (and))))
+(check-exn exn:fail? (thunk (eq? (cdr '(soured milk)) 'milk)))
 
-(cdr '(a b c))
-
-(cdr '((a b c) x y z))
-
-(cdr '(hamburger))
-
-(cdr '((x) t r))
-
-;**********************************************************
-;The Law of Cdr
-;
-;The primitive cdr is defined only for non-empty lists.
-;The cdr of any non-empty list is always another list.
-;**********************************************************
-
-;;stuff
-
-;**********************************************************
-;The Law of Cons
-;
-;The primitive cons takes two arguments. The second argument
-;to cons must be a list. The result is a list.
-;**********************************************************
-
-;;stuff
-
-;**********************************************************
-;The Law of Null
-;
-;The primitive null? is defined only for lists.
-;**********************************************************
-
-(atom? 'Harry)
-
-(atom? '(Harry had a heap of apples))
-
-(atom? (car '(Harry had a heap of apples)))
-
-(atom? (cdr '(Harry had a heap of apples)))
-
-(atom? (cdr '(Harry)))
-
-(atom? (car (cdr '(swing low sweet cherry oat))))
-
-(atom? (car (cdr '(swing (low sweet) cherry oat))))
-
-(eq? 'Harry 'Harry)
-
-(eq? 'margarine 'butter)
-
-;**********************************************************
-;The Law of Eq?
-;
-;The primitive eq? takes two arguments. Each must be
-;a non-numeric atom.
-;**********************************************************
-
-(eq? (car '(Mary had a little lamb chop)) 'Mary)
-
-(eq? (car '(beans beans we need jelly beans)) (car (cdr '(beans beans we need jelly beans))))
+(check-true (eq? (car '(beans beans we need jelly beans))
+                 (car (cdr '(beans beans we need jelly beans)))))
--- source/the-little-schemer.rkt
+++ source/the-little-schemer.rkt
@@ -1,11 +1,44 @@
 #lang racket
 
+;; Used to redefine Racket functions in terms of the original functions
+(require (only-in racket
+                  (cons racket-cons)
+                  (null? racket-null?)
+                  (eq? racket-eq?)))
+
 (provide (all-defined-out))
 
-(define atom?
-  (lambda (x)
-    (and (not (pair? x)) (not (null? x)))))
+;; Predicate for determining if a value is an atom or not.
+;; The definition of this is found in the preface.
+(define (atom? x)
+  (and (not (pair? x)) (not (racket-null? x))))
+;; Note that we need to use the racket-null? and not our newly contracted null?
+;; becuase x may be an atom, which racket-null? supports and null? (as defined
+;; in the book and thus our contracted version) does not.
 
+(define (s-exp? x)
+  (or (atom? x) (list? x)))
+
+;; Provide a cons as defined in the book such that it requires a list as
+;; the second argument. This is enforced using Racket's contract system.
+;; Racket's cons works on any values, as mentioned in the footnote on page 8.
+(define/contract (cons s l)
+  (-> any/c list? list?)
+  (racket-cons s l))
+
+;; Provide a cons as defined in the book such that it requires a list as
+;; the argument. See the footnote on page 10.
+(define/contract (null? l)
+  (-> list? boolean?)
+  (racket-null? l))
+
+;; Provide an eq? as defined in the book such that it requires a non-numeric
+;; atom for each argument. See the footnotes on page 12.
+(define/contract (eq? a b)
+  (-> (and/c atom? (not/c number?)) (and/c atom? (not/c number?)) boolean?)
+  (racket-eq? a b))
+
+;; Predicate for deterimining if a value is a
 (define lat?
   (lambda (l)
     (cond
@@ -17,13 +50,35 @@
   (lambda (a lat)
     (cond
       [(null? lat) #f]
-      [else (or (eq? (car lat) a)
+      [else (or (racket-eq? (car lat) a)
                 (member? a (cdr lat)))])))
 
 (define rember
   (lambda (a lat)
     (cond
       [(null? lat) '()]
-      [(eq? a (car lat)) (cdr lat)]
-      [else (cons (car lat)
-                  (rember a (cdr lat)))])))
+      [(racket-eq? a (car lat)) (cdr lat)]
+      [else (racket-cons (car lat)
+                         (rember a (cdr lat)))])))
+
+(module+ test
+  (require rackunit)
+
+  (check-false (atom? (quote ())))
+
+  (check-false (atom? '()))
+
+  (check-equal? (cons 'a '()) '(a))
+
+  (check-equal? (cons 'a '(b)) '(a b))
+
+  (check-exn exn:fail? (thunk(cons 'a 'b)))
+
+  (check-pred null? '())
+
+  (check-pred null? (quote ()))
+
+  (check-pred null? empty)
+
+  (check-exn exn:fail? (thunk (null? 'a)))
+  )
